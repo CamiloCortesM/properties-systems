dba8ae75dadbc1f011a8348b083ae37461776cf5
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-25 16:56:05 -0500

    initial data

---
+++ src/helpers/initialData.js
@@ -0,0 +1,46 @@
+export const initialData = () => {
+  return [
+    {
+      name: "One Small trip",
+      id:1,
+      attributes: {
+        description: "Juega por primera vez cualquier juego",
+        complete: false,
+        progress: 0,
+        url: "image/url.png",
+      },
+    },
+    {
+      name: "Style Points",
+      id:2,
+      attributes: {
+        description: "Completa el quiz de propiedades de los sistemas",
+        complete: false,
+        progress: 0,
+        url: "image/url.png",
+      },
+    },
+
+    {
+      name: "Top Tier",
+      id:3,
+      attributes: {
+        description: "Acaba con mayor puntuacion el Quiz de propiedades de los sistemas",
+        complete: false,
+        progress: 0,
+        url: "image/url.png",
+      },
+    },
+
+    {
+      name: "Infallible",
+      id:4,
+      attributes: {
+        description: "Completa el modo de juego Wordle",
+        complete: false,
+        progress: 0,
+        url: "image/url.png",
+      },
+    },
+  ];
+};
