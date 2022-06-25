725e6f2163d8f544817627633ffa08723048ab9e
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-25 02:16:56 -0500

    sidebar configuration

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -7,30 +7,43 @@ import AbcIcon from "@mui/icons-material/Abc";
 import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import { Link } from "@mui/material";
-import GitHubIcon from '@mui/icons-material/GitHub';
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export const MainListItems = () => {
   return (
     <>
-      <Link component={RouterLink} color="inherit" to="/wordle" sx={{
-        textDecoration: 0
-      }} >
-      <ListItemButton>
-        <ListItemIcon>
-          <AbcIcon />
-        </ListItemIcon>
-        <ListItemText primary="Wordle" />
-      </ListItemButton>
+      <ListSubheader component="div" inset>
+        Juegos
+      </ListSubheader>
+      <Link
+        component={RouterLink}
+        color="inherit"
+        to="/wordle"
+        sx={{
+          textDecoration: 0,
+        }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <AbcIcon />
+          </ListItemIcon>
+          <ListItemText primary="Wordle" />
+        </ListItemButton>
       </Link>
-      <Link component={RouterLink} to="/quiz" color="inherit" sx={{
-        textDecoration: 0
-      }}>
-      <ListItemButton>
-        <ListItemIcon>
-          <VideogameAssetIcon />
-        </ListItemIcon>
-        <ListItemText primary="Quiz" />
-      </ListItemButton>
+      <Link
+        component={RouterLink}
+        to="/quiz"
+        color="inherit"
+        sx={{
+          textDecoration: 0,
+        }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <VideogameAssetIcon />
+          </ListItemIcon>
+          <ListItemText primary="Quiz" />
+        </ListItemButton>
       </Link>
     </>
   );
@@ -39,28 +52,34 @@ export const MainListItems = () => {
 export const SecondaryListItems = () => {
   return (
     <>
-      <ListSubheader component="div" inset>
-        Perfil
-      </ListSubheader>
-      <Link component={RouterLink} to="/mission" color="inherit" sx={{
-        textDecoration: 0
-      }}>
-      <ListItemButton>
-        <ListItemIcon>
-          <EmojiEventsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Misiones" />
-      </ListItemButton>
+      <Link
+        component={RouterLink}
+        to="/mission"
+        color="inherit"
+        sx={{
+          textDecoration: 0,
+        }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <EmojiEventsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Logros" />
+        </ListItemButton>
       </Link>
-      <Link href="https://github.com/CamiloCortesM/tgs-games" color="inherit" sx={{
-        textDecoration: 0
-      }}>
-      <ListItemButton>
-        <ListItemIcon>
-          <GitHubIcon  />
-        </ListItemIcon>
-        <ListItemText primary="Github" />
-      </ListItemButton>
+      <Link
+        href="https://github.com/CamiloCortesM/tgs-games"
+        color="inherit"
+        sx={{
+          textDecoration: 0,
+        }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <GitHubIcon />
+          </ListItemIcon>
+          <ListItemText primary="Github" />
+        </ListItemButton>
       </Link>
     </>
   );
