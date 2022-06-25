c10d936fe99f851c9c8d94d193afb8ffd32219c2
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-25 02:16:22 -0500

    creation of achievement cards

--- src/pages/Mission.jsx
+++ src/pages/Mission.jsx
@@ -1,4 +1,5 @@
-import { Grid } from "@mui/material";
+import { Divider, Grid, Typography } from "@mui/material";
+import { CardAchievement } from "../components/CardAchievement";
 import { HeaderLayout } from "../layout/HeaderLayout";
 
 export const Mission = () => {
@@ -11,10 +12,35 @@ export const Mission = () => {
           p: 2,
         }}
       >
-        <Grid item xs={6} md={3} sx={{
-            mt:2
-        }}>
-          hola
+        <Grid
+          item
+          xs={12}
+          md={12}
+          sx={{
+            mt: 2,
+          }}
+        >
+          <Typography
+            component="h1"
+            variant="strong"
+            sx={{
+              textAlign: "center",
+              textTransform: "capitalize",
+            }}
+          >
+            LOGROS
+          </Typography>
+          <Divider />
+        </Grid>
+        <Grid container 
+        justifyContent="space-around"
+        alignItems="center" sx={{
+          mt:3
+        }}
+        >
+          <Grid xs={12} md={3} item>
+              <CardAchievement />
+          </Grid>
         </Grid>
       </Grid>
     </HeaderLayout>
