d7aaef22dc2f5ae528ffb0ac709c38ffe5e2144c
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-26 01:03:20 -0500

    questions for the quiz

---
+++ src/data/questions.js
@@ -0,0 +1,38 @@
+export const questions = [
+    {
+        tittle: "¿pregunta 1?",
+        options: [
+          { textResponse: "A", isCorrect: true },
+          { textResponse: "B", isCorrect: false },
+          { textResponse: "C", isCorrect: false },
+          { textResponse: "D", isCorrect: false },
+        ],
+      },
+      {
+        tittle: "pregunta 2?",
+        options: [
+          { textResponse: "A", isCorrect: false },
+          { textResponse: "B", isCorrect: true },
+          { textResponse: "C", isCorrect: false },
+          { textResponse: "D", isCorrect: false },
+        ],
+      },
+      {
+        tittle: "¿pregunta 3?",
+        options: [
+          { textResponse: "A", isCorrect: true },
+          { textResponse: "B", isCorrect: false },
+          { textResponse: "C", isCorrect: false },
+          { textResponse: "D", isCorrect: false },
+        ],
+      },
+      {
+        tittle: "¿pregunta 4?",
+        options: [
+          { textResponse: "A", isCorrect: false },
+          { textResponse: "B", isCorrect: false },
+          { textResponse: "C", isCorrect: false },
+          { textResponse: "D", isCorrect: true },
+        ],
+      },
+]
