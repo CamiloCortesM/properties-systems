570aad0b90d085ce9828325611fc9e6d3cf29233
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-06-26 01:04:08 -0500

    sample questions

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,9 +1,110 @@
-import { HeaderLayout } from "../layout/HeaderLayout"
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { HeaderLayout } from "../layout/HeaderLayout";
 
 export const Quiz = () => {
   return (
     <HeaderLayout>
-      <div>Quiz</div>
+      <Grid
+        container
+        direction="row"
+        sx={{
+          p: 2,
+        }}
+      >
+        <Grid xs={12} item>
+          <Card
+            sx={{
+              width: "100%",
+              m: "auto",
+            }}
+          >
+            <CardHeader title="1/5" sx={{
+              backgroundColor:"#ede7f6",
+            }} />
+            <CardContent
+              sx={{
+                backgroundColor: "#ede7f6",
+              }}
+            >
+              <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                direction="column"
+              >
+                <Toolbar sx={{
+                  height:200
+                }}>
+                  <Typography
+                    variant="h6"
+                    color="primary"
+                  >
+                    ¿Esta es la preguntaasda sdsadasdsada sdadas dasdasd?
+                  </Typography>
+                </Toolbar>
+              </Grid>
+            </CardContent>
+            <CardContent
+              sx={{
+                backgroundColor: "#ede7f6",
+              }}
+            >
+              <Grid container direction="row" spacing={1}>
+                <Grid item lg={6} >
+                  <Button variant="contained" color="primary" fullWidth sx={{
+                    height:100
+                  }}>
+                    sadsa
+                  </Button>
+                </Grid>
+                <Grid item lg={6}>
+                  <Button variant="contained" color="primary" fullWidth sx={{
+                    height:100
+                  }}>
+                    sadsa
+                  </Button>
+                </Grid>
+                <Grid item lg={6}>
+                  <Button variant="contained" color="primary" fullWidth sx={{
+                    height:100
+                  }}>
+                    sadsa
+                  </Button>
+                </Grid>
+                <Grid item lg={6}>
+                  <Button variant="contained" color="primary" fullWidth sx={{
+                    height:100
+                  }}>
+                    sadsa
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+            <CardContent
+              sx={{
+                backgroundColor: "#ede7f6",
+              }}
+            >
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "primary",
+                }}
+              >
+                Hola
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </HeaderLayout>
-  )
-}
+  );
+};
