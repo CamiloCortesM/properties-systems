308e38c9fd06be66229d19a36355dbe70c186316
AndresfelipePa <108076867+AndresfelipePa@users.noreply.github.com>
2022-06-27 23:39:49 -0500

    Update questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -9,30 +9,30 @@ export const questions = [
         ],
       },
       {
-        tittle: "pregunta 2?",
+        tittle: "Segun ______, Se puede hablar de una filosifia de sistemas, ya que toda teoria cientifica de gran alcance tiene aspectos metafisicos?",
         options: [
-          { textResponse: "A", isCorrect: false },
-          { textResponse: "B", isCorrect: true },
-          { textResponse: "C", isCorrect: false },
-          { textResponse: "D", isCorrect: false },
+          { textResponse: "Kenneth Boulding", isCorrect: false },
+          { textResponse: "Ludwing von Bertanlanffy", isCorrect: true },
+          { textResponse: "Norbert Wiener", isCorrect: false },
+          { textResponse: "Anatol Rapoport", isCorrect: false },
         ],
       },
       {
-        tittle: "¿pregunta 3?",
+        tittle: "¿Cuantas partes conforman un sistema?",
         options: [
-          { textResponse: "A", isCorrect: true },
-          { textResponse: "B", isCorrect: false },
-          { textResponse: "C", isCorrect: false },
-          { textResponse: "D", isCorrect: false },
+          { textResponse: "5", isCorrect: true },
+          { textResponse: "4", isCorrect: false },
+          { textResponse: "3", isCorrect: false },
+          { textResponse: "6", isCorrect: false },
         ],
       },
       {
-        tittle: "¿pregunta 4?",
+        tittle: "¿Segun la lectura, cuales son los tres tipos de sistemas que encontramos a diario en  nuestras vidas?",
         options: [
-          { textResponse: "A", isCorrect: false },
-          { textResponse: "B", isCorrect: false },
-          { textResponse: "C", isCorrect: false },
-          { textResponse: "D", isCorrect: true },
+          { textResponse: "Biologico,reales y cerrados", isCorrect: false },
+          { textResponse: "Reales,abstractos y abiertos", isCorrect: false },
+          { textResponse: "Informatico, operativo y dinamico", isCorrect: false },
+          { textResponse: "Naturales,artificiales y compuestos", isCorrect: true },
         ],
       },
       {
@@ -45,16 +45,16 @@ export const questions = [
         ],
       },
       {
-        tittle: "¿pregunta 6?",
+        tittle: "¿Cuantos niveles jerarquicos tienen los sistemas?",
         options: [
-          { textResponse: "A", isCorrect: false },
-          { textResponse: "B", isCorrect: false },
-          { textResponse: "C", isCorrect: false },
-          { textResponse: "D", isCorrect: true },
+          { textResponse: "3", isCorrect: false },
+          { textResponse: "5", isCorrect: false },
+          { textResponse: "7", isCorrect: false },
+          { textResponse: "9", isCorrect: true },
         ],
       },
       {
-        tittle: "¿pregunta 7?",
+        tittle: "¿?",
         options: [
           { textResponse: "A", isCorrect: true },
           { textResponse: "B", isCorrect: false },
