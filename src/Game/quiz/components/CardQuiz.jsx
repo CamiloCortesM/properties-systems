f9e666e95e7f2b567533be06b48dabd3468d5c03
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-07-02 23:47:45 -0500

    implemented the logic to unlock achievements and save them in local storage

--- src/Game/quiz/components/CardQuiz.jsx
+++ src/Game/quiz/components/CardQuiz.jsx
@@ -7,14 +7,17 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import { useEffect, useMemo, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { questions } from "../../../data/questions";
 import { ButtonAnswer } from "./ButtonAnswer";
 import { ProgressBar } from "react-bootstrap";
 import { getUrlMedal } from "../../../helpers/getUrlMedal";
 import { QuizResult } from "../views/QuizResult";
+import { ContextArchi } from "../../../hooks/ContextArchi";
+import { getArchiByName } from "../../../selectors/getArchiByName";
+import { getAchvtById } from "../../../selectors/getAchvtById";
 
-export const CardQuiz = ({setRun}) => {
+export const CardQuiz = ({ setRun }) => {
   const [isFinished, setIsFinished] = useState(false);
   const [score, setScore] = useState(0);
   const [questionCurrent, setQuestionCurrent] = useState(0);
@@ -22,9 +25,70 @@ export const CardQuiz = ({setRun}) => {
   const [areDisabled, setAreDisabled] = useState(false);
   const [color, setColor] = useState({});
 
+  const { achvt, setAchvt } = useContext(ContextArchi);
+
+  const OneSmall = useMemo(() => getAchvtById(1, achvt), [achvt]);
+  const StylePoints = useMemo(() => getAchvtById(2, achvt), [achvt]);
+  const TopTier = useMemo(() => getAchvtById(3, achvt), [achvt]);
+
   const url = useMemo(() => getUrlMedal(score), [score]);
 
+
+  if (!TopTier.attributes.complete && score === 10) {
+    const data = getArchiByName(TopTier.name, achvt);
+    setAchvt([
+      ...data,
+      {
+        name: TopTier.name,
+        id: TopTier.id,
+        attributes: {
+          description: TopTier.attributes.description,
+          complete: true,
+          progress: 100,
+          url: TopTier.attributes.url,
+        },
+      },
+    ]);
+  }
+
   const handleAnswerSubmit = (isCorrect, e) => {
+    if (!OneSmall.attributes.complete) {
+      const data = getArchiByName(OneSmall.name, achvt);
+      setAchvt([
+        ...data,
+        {
+          name: OneSmall.name,
+          id: OneSmall.id,
+          attributes: {
+            description: OneSmall.attributes.description,
+            complete: true,
+            progress: 100,
+            url: OneSmall.attributes.url,
+          },
+        },
+      ]);
+    }
+
+    if (!StylePoints.attributes.complete) {
+      const data = getArchiByName(StylePoints.name, achvt);
+      setAchvt([
+        ...data,
+        {
+          name: StylePoints.name,
+          id: StylePoints.id,
+          attributes: {
+            description: StylePoints.attributes.description,
+            complete: questionCurrent == 10 ? true : false,
+            progress:
+              (questionCurrent + 1) * 10 < StylePoints.attributes.progress
+                ? StylePoints.attributes.progress
+                : (questionCurrent + 1) * 10,
+            url: StylePoints.attributes.url,
+          },
+        },
+      ]);
+    }
+
     if (isCorrect && !areDisabled) {
       setScore(score + 1);
       setColor({
@@ -57,10 +121,7 @@ export const CardQuiz = ({setRun}) => {
     }
   }, [timeRestant, isFinished]);
 
-  if (isFinished)
-    return (
-      <QuizResult setRun={setRun} score={score} url={url}/>
-    );
+  if (isFinished) return <QuizResult setRun={setRun} score={score} url={url} />;
 
   return (
     <Card
