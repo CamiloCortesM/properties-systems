d4d31899683ec6c17a5e929508155b2ce78561a9
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-07-04 02:26:25 -0500

    array of objects to use in the wordle game

---
+++ src/data/word.js
@@ -0,0 +1,26 @@
+export const words =[
+    {
+        name:"Armonia",
+        description:"Propiedad que mide el nivel de compatibilidad con su medio o contexto.proceso o características en la medida que el medio se lo exige y es estático cuando el medio también lo es."
+    },
+    {
+        name:"Contexto",
+        description:"El conjunto de objetos exteriores al sistema, pero que influyen decididamente en éste, y a su vez el sistema influye en ellos."
+    },
+    {
+        name:"Energia",
+        description:"Entradas al sistema o resultados de procesos que hacen que el sistema continue su comportamiento o realice cambios de acuerdo a los cambios que sufra."
+    },
+    {
+        name:"Proceso",
+        description:"Transformación de una entrada en una salida, como tal puede ser una máquina, un individuo, un computador, un producto químico, una tarea realizada por un miembro de la organización, etcétera."
+    },
+    {
+        name:"Salidas",
+        description:"Son los resultados que se obtienen de procesar las entradas. Al igual que las entradas, pueden adoptar la forma de productos, servicios e información."
+    },
+    {
+        name:"Sistema",
+        description:"Es un conjunto organizado de cosas o partes interactuantes e interdependientes, que se relacionan formando un todo unitario y complejo."
+    },
+]
