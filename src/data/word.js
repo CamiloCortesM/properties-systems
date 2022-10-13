b2271b75650e30e60fadc784e3529e2285455ca2
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-10-12 23:41:44 -0500

    a new element has been added for feedback

--- src/data/word.js
+++ src/data/word.js
@@ -3,25 +3,30 @@ export const words = [
     name: "Armonia",
     description:
       "Propiedad que mide el nivel de compatibilidad con su medio o contexto.proceso o características en la medida que el medio se lo exige y es estático cuando el medio también lo es.",
+    video: 'https://www.laverdad.es/ababol/ciencia/201311/02/teoria-de-sistemas-o-teoria-de-la-armonia.html?ref=https%3A%2F%2Fwww.google.com%2F'
   },
   {
     name: "Energia",
     description:
       "Entradas al sistema o resultados de procesos que hacen que el sistema continue su comportamiento o realice cambios de acuerdo a los cambios que sufra.",
+    video: 'https://www.moebio.uchile.cl/03/frprinci.html'
   },
   {
     name: "Proceso",
     description:
       "Transformación de una entrada en una salida, como tal puede ser una máquina, un individuo, un computador, un producto químico, una tarea realizada por un miembro de la organización, etcétera.",
+    video: 'https://www.youtube.com/watch?v=PpfawGVcUBc'
   },
   {
     name: "Salidas",
     description:
       "Son los resultados que se obtienen de procesar las entradas. Al igual que las entradas, pueden adoptar la forma de productos, servicios e información.",
+    video: 'https://www.monografias.com/trabajos5/teorsist/teorsist'
   },
   {
     name: "Sistema",
     description:
       "Es un conjunto organizado de cosas o partes interactuantes e interdependientes, que se relacionan formando un todo unitario y complejo.",
+    video: 'https://www.youtube.com/watch?v=QbBy20GPlqc'
   },
 ];
